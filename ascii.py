0c786fe8d09c6e634e657ec1c27ea62760b115f4
Leila Hejazi <lhej@bn.co>
2015-12-21 18:06:41 -0800

    cleaned up ascii generator file

--- ascii.py
+++ ascii.py
@@ -1,13 +1,15 @@
 from PIL import Image
 from math import ceil
 
-# resize new image to fit
+# resize new image to provided new_width using aspect ratio
 def size_image(image, new_width=500):
     original_width, original_height = image.size
     aspect_ratio = original_height/float(original_width)
     new_height = int(aspect_ratio*new_width)
     return image.resize((new_width, new_height))
 
+# assigns a shade on the gray scale to an ascii value, determined by 
+# provided character set
 def shade_to_ascii(chars):
     d = {}
     for x in xrange(0,256):
@@ -15,10 +17,14 @@ def shade_to_ascii(chars):
         d[x] = chars[x/char_buckets]
     return d
 
+# The bellow set of chars gives you a really sharp ascii photo
 chars = ['#','@','%','0','M','8','B','&','$','o','r','=','*','+','x','|',':','\"',',','.']
-asciis = shade_to_ascii(chars)
+# All of the chars in "The Americans, I'll be yours"
+chars1 = list("#MNHAERSCTI!lbhmaeosucntri*+:\',.")
 
-# Assign chars
+asciis = shade_to_ascii(chars1)
+
+# Assign chars to different colored pixels
 def pixels_to_ascii(image_data, width):
     new_image = []
     for x in xrange(0, len(image_data), width):
@@ -28,7 +34,7 @@ def pixels_to_ascii(image_data, width):
         new_image.append("".join(tmp))
     return "\n".join(new_image)
 
-# Save image
+# Save image by writing data to file
 def write_to_file(new_image, filename):
     with open(filename, 'wb') as fp:
         fp.write(new_image)
@@ -36,6 +42,16 @@ def write_to_file(new_image, filename):
 if __name__ == '__main__':
     # To generate ascii art from a photo, run the following:
     # $ python ascii.py <input-image> [output-filename] [width]
+    #
+    #   ARGUMENTS:
+    #       input-image(required):      the image file you use to make ascii art
+    #
+    #       output-filename(optional):  the filename you'll give your new ascii art. By 
+    #                                   default 'out.txt' is used
+    #
+    #       width(optional):            an adjusted width of your image. The height will be adjusted
+    #                                   to specified width using the aspect ratio. Original image
+    #                                   height is used by default
 
     import sys
     
